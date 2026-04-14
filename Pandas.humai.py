48bfaa8eb7bb8b218394b186bcfbeb4ebab25adf
nilander1 <102563137+nilander1@users.noreply.github.com>
2026-04-14 20:45:58 -0300

    Delete Pandas.humai.py

--- Pandas.humai.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv('http://cdn.buenosaires.gob.ar/datosabiertos/datasets/sueldo-funcionarios/sueldo_funcionarios_2019.csv')
-print (df.sample(5))
